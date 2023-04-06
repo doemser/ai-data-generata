6209ac66064608be2229162ef6d9fa09657bfa37
Dominik Hautau <dominikhautau@gmail.com>
2023-04-06 19:42:01 +0200

    fixed json copy to clipboard

--- src/components/CopyToClipboard/index.tsx
+++ src/components/CopyToClipboard/index.tsx
@@ -1,13 +1,15 @@
 import Button from "@mui/material/Button";
+import useStore from "../../hooks/useStore";
 
 export default function CopyToClipboard() {
+  const answer = useStore((state) => state.answer);
   return (
     <Button
       type="button"
       variant="outlined"
       onClick={() => {
         // navigator.clipboard.writeText() is an async function we have to wait for
-        navigator.clipboard.writeText(answer?.answer.content).then(
+        navigator.clipboard.writeText(answer?.content).then(
           () => {
             alert(`copied JSON to the clipboard`);
           },
